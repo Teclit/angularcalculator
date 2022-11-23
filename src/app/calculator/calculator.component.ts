3b71d01028445c6a470b694cda5429347924eb68
Teklit <teklitte@gmail.com>
2022-11-23 09:09:27 +0100

    project template

---
+++ src/app/calculator/calculator.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
+
+
+export class CalculatorComponent {
+  
+  title = "Simple Angular Project *Calculator"
+  author ="Teclit"
+
+}
